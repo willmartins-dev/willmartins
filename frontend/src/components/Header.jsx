48b4af4c16671f5e5ef02a0ae7f3b63e23a305fd
willmartins-dev <willmartinsdeveloper@hotmail.com>
2025-04-24 17:39:21 -0300

    Primeiro Commit + header

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="shadow-md">
+      <div className="flex items-center justify-between py-4 px-4 max-w-7xl mx-auto">
+        <div className="text-3xl text-primary-600">Personal Trainer</div>
+        <div className="p-2 border border-gray-400 rounded-full flex items-center gap-2">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            className="size-8"
+          >
+            <path
+              fillRule="evenodd"
+              d="M3 6.75A.75.75 0 0 1 3.75 6h16.5a.75.75 0 0 1 0 1.5H3.75A.75.75 0 0 1 3 6.75ZM3 12a.75.75 0 0 1 .75-.75h16.5a.75.75 0 0 1 0 1.5H3.75A.75.75 0 0 1 3 12Zm0 5.25a.75.75 0 0 1 .75-.75h16.5a.75.75 0 0 1 0 1.5H3.75a.75.75 0 0 1-.75-.75Z"
+              clipRule="evenodd"
+            />
+          </svg>
+
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            className="size-8 text-gray-400"
+          >
+            <path
+              fillRule="evenodd"
+              d="M18.685 19.097A9.723 9.723 0 0 0 21.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 0 0 3.065 7.097A9.716 9.716 0 0 0 12 21.75a9.716 9.716 0 0 0 6.685-2.653Zm-12.54-1.285A7.486 7.486 0 0 1 12 15a7.486 7.486 0 0 1 5.855 2.812A8.224 8.224 0 0 1 12 20.25a8.224 8.224 0 0 1-5.855-2.438ZM15.75 9a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z"
+              clipRule="evenodd"
+            />
+          </svg>
+          <p>Willian M.</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
